f951020e9564e4204304521f155dd91959d26755
eljudepanao <eljudepanao03@gmail.com>
2024-08-11 01:37:30 +0800

    debug

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -87,29 +87,29 @@ export async function POST(req: Request) {
   }
 
   // UPDATE
-  if (eventType === "user.updated") {
-    const { id, image_url, first_name, last_name, username } = evt.data;
+  // if (eventType === "user.updated") {
+  //   const { id, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
-      firstName: first_name,
-      lastName: last_name,
-      username: username!,
-      photo: image_url,
-    };
+  //   const user = {
+  //     firstName: first_name,
+  //     lastName: last_name,
+  //     username: username!,
+  //     photo: image_url,
+  //   };
 
-    const updatedUser = await updateUser(id, user);
+  //   const updatedUser = await updateUser(id, user);
 
-    return NextResponse.json({ message: "OK", user: updatedUser });
-  }
+  //   return NextResponse.json({ message: "OK", user: updatedUser });
+  // }
 
   // DELETE
-  if (eventType === "user.deleted") {
-    const { id } = evt.data;
+  // if (eventType === "user.deleted") {
+  //   const { id } = evt.data;
 
-    const deletedUser = await deleteUser(id!);
+  //   const deletedUser = await deleteUser(id!);
 
-    return NextResponse.json({ message: "OK", user: deletedUser });
-  }
+  //   return NextResponse.json({ message: "OK", user: deletedUser });
+  // }
 
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
