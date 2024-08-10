fe4292e42b9f1f66b0476501d00d82ff18a7a5b0
eljudepanao <eljudepanao03@gmail.com>
2024-08-10 23:58:01 +0800

    add header for Transformation types

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -16,11 +16,11 @@ if(!cached) {
 
 
 export const connectToDatabase = async () => {
-    if(!cached.conn) return cached.conn;
+    if(cached.conn) return cached.conn;
 
     if(!MONGODB_URL) throw new Error('Missing MONGODB_URL')
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {dbName: 'CreatiPix', bufferCommands: false})
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {dbName: 'creatipix', bufferCommands: false, connectTimeoutMS: 30000})
 
     cached.conn = await cached.promise
 
